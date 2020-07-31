12e60d66d883cb143c13bba1af0c676d6560d084
aparatowo <kontakt@aparatowo.pl>
2020-08-01 00:09:06 +0200

    zmiany w bazie, kilka nowych funkcji, a w tym odczyt daty i zapis dni do urodzin do bazy

--- models.py
+++ models.py
@@ -4,35 +4,25 @@ from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__= 'users'
+class Person(Base):
+    __tablename__ = 'persons'
 
     id = Column(Integer(), primary_key=True, unique=True, autoincrement=True)
     gender = Column(String(50), nullable=False)
     title = Column(String(50), nullable=False)
     first_name = Column(String(256), nullable=False)
     last_name = Column(String(256), nullable=False)
-    true_id_name = Column(String(50))
-    true_id_value = Column(String(50))
+    id_name = Column(String(50))
+    id_value = Column(String(50))
     nat = Column(String(50))
     dob = Column(String(50), nullable=False) #day of birth
     age = Column(Integer(), nullable=False)
     dtb = Column(String(50), nullable=False) #days to birthday
     register_date = Column(String(50), nullable=False)
     registered_at_age = Column(Integer())
-
-    _confidential = relationship("Confidential")
-    contact_details = relationship("ContactDetails")
-    location = relationship("Location")
-
-
-class Confidential(Base):
-    __tablename__ = 'confidencial'
-    id = Column(Integer(), primary_key=True, unique=True)
-    user_ID = Column(Integer(), ForeignKey('users.id'))
-    id_name = Column(String())
-    id_value = Column(String())
-    uuid = Column(String())
+    id_name = Column(String(50))
+    id_value = Column(String(50))
+    uuid = Column(String(256))
     username = Column(String())
     password = Column(String())
     pass_strength = Column(Integer())
@@ -40,21 +30,9 @@ class Confidential(Base):
     md5 = Column(String())
     sha1 = Column(String())
     sha256 = Column(String())
-
-
-class ContactDetails(Base):
-    __tablename__ = 'contact_details'
-    id = Column(Integer(), primary_key=True, unique=True)
-    user_ID = Column(Integer(), ForeignKey('users.id'))
     email = Column(String(), nullable=False)
     phone = Column(Integer())
     cellphone = Column(Integer())
-
-
-class Location(Base):
-    __tablename__ = 'location'
-    id = Column(Integer(), primary_key=True, unique=True)
-    user_ID = Column(Integer(), ForeignKey('users.id'))
     street_name = Column(String())
     home_number = Column(String())
     city = Column(String())
--- tools.py
+++ tools.py
@@ -2,6 +2,7 @@ from sqlalchemy import event, create_engine
 from sqlalchemy.orm import sessionmaker
 from models import *
 from json import load
+from datetime import datetime as dt
 
 
 # from string import ascii_lowercase, ascii_uppercase, digits, punctuation
@@ -24,13 +25,59 @@ def get_session(echo=False):
 def fill_up_base():
     conn = get_session()
     persons_dict = None
+    persons_list = []
 
     with open("persons.json", encoding="utf8") as persons_json:
         persons_dict = load(persons_json)
 
     for result in persons_dict['results']:
-        result
+        normalised_result(result)
+        result.append(Person())
 
+def normalised_person_data(data):
+    gender = data['gender']
+    title = data['name']['title']
+    first_name = data['name']['first']
+    last_name = data['name']['last']
+    id_name = data['id']['name']
+    id_value = data['id']['value']
+    nat = data['nat']
+    dob = data['dob']['date']
+    age = data['dob']['age']
+    dtb = days_till_bd(dob)
+    uuid = data['login']['uuid']
+    username = data['login']['username']
+    password = data['login']['password']
+    pass_strength = pass_strength_score(password)
+    salt = data['login']['salt']
+    md5 = data['login']['md5']
+    sha1 = data['login']['sha1']
+    sha256 = data['login']['sha256']
+    email = data['email']
+    phone = data['phone']
+    cellphone = data['cell']
+    street_name = data['location']['street']['name']
+    home_number = data['location']['street']['number']
+    city = data['location']['city']
+    postcode = data['location']['postcode']
+    state = data['location']['state']
+    country = data['location']['country']
+    latitude = data['location']['coordinates']['latitude']
+    longitude = data['location']['coordinates']['longitude']
+    timezone_offset = data['location']['timezone']['offset']
+    timezone_desc = data['location']['timezone']['description']
+
+def str_to_date(date, skip_char=1):
+    return dt.fromisoformat(date[0:-skip_char])
+
+def days_till_bd(date):
+    now = dt.now()
+    dob = str_to_date(date)
+    this_year_bd = dt(now.year, dob.month, dob.day)
+    next_year_bd = dt(now.year+1, dob.month, dob.day)
+    delta = [(this_year_bd - now), (next_year_bd - now)]
+    delta = max(delta)
+    return delta.days
 
 
 
